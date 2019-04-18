b7360916fd0f20ab542327ad23d08416ec94047f
puqiansheng <puqiansheng@daokoudai.com>
2019-04-18 19:46:18 +0800

    --

---
+++ src/test/java/MainDemoTest.java
@@ -0,0 +1,1 @@
+public class MainDemoTest {    @org.junit.Before    public void setUp() throws Exception {    }    @org.junit.After    public void tearDown() throws Exception {    }    @org.junit.Test    public void test() {        MainDemo mainDemo = new MainDemo();        mainDemo.test1();    }}
